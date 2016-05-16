fe688fac8696c05e02b1b6294f1d6afecb655d1f
Brady Smith <brady_smith@outlook.com>
2016-05-16 07:20:00 +0000

    Updated spacing

--- rewind.py
+++ rewind.py
@@ -3,26 +3,28 @@ from multiprocessing import Process, Value, Array
 import numpy as np
 import cv2
 
+
 def getFramesLoop(count, loopBool):
-	while loopBool:
-	   # Capture frame-by-frame
-	   ret, frame = cap.read()
-	   count.value = count.value + 1 
-	   index = count.value
+    while loopBool:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        count.value = count.value + 1
+        index = count.value
 
-	   name = "frames/frame%d.jpg"%index
-	   cv2.imwrite(name, frame)  
-	   print index 
+        name = "frames/frame%d.jpg" % index
+        cv2.imwrite(name, frame)
+        print index
 
-	   if (index > 10):
-	   		loopBool = False
+        if (index > 10):
+            loopBool = False
+
 
 if __name__ == '__main__':
-	cap = cv2.VideoCapture(0)
-	loopBool = Value('b', True)
-	count = Value('d', 0.0)
+    cap = cv2.VideoCapture(0)
+    loopBool = Value('b', True)
+    count = Value('d', 0.0)
     # arr = Array('i', range(10))
 
-	p = Process(target=getFramesLoop, args=(count, loopBool))
-	p.start()
-	p.join()
+    p = Process(target=getFramesLoop, args=(count, loopBool))
+    p.start()
+    p.join()
