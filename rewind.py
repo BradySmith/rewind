3a43ba359abd24e392c6439fe15537243391d44f
Brady Smith <brady_smith@outlook.com>
2016-05-18 01:34:07 +0000

    Camera now has frames that are added after

--- rewind.py
+++ rewind.py
@@ -1,8 +1,9 @@
 import cv2
 import subprocess
-import os, shutil
+import os
 
-FRAMES_TO_KEEP = 10
+FRAMES_TO_KEEP_BEFORE = 10
+FRAMES_TO_KEEP_AFTER = 10
 
 
 def removeAllFilesInFolder(folder):
@@ -11,6 +12,7 @@ def removeAllFilesInFolder(folder):
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
+
         except Exception as e:
             print(e)
 
@@ -22,33 +24,44 @@ def getFramesLoop():
     while True:
         try:
             try:
-                os.remove("frames/frame%d.jpg" % (index - FRAMES_TO_KEEP))
+                os.remove("frames/frame%d.jpg" % (index - FRAMES_TO_KEEP_BEFORE))
             except OSError:
                 pass
 
-            ret, frame = cap.read()
-            name = "frames/frame%d.jpg" % index
-            cv2.imwrite(name, frame)
+            getFrame(index, cap)
             index += 1
 
-            # Reset index when the number gets too big
-            if (index == 100000000):
+            # Reset index when the number gets too big.
+            if index == 100000000:
                 index = 0
 
         except KeyboardInterrupt:
+            # Take a few more frames.
+            print "Interrupt detected, taking the after frames."
+            for i in range(index, index + FRAMES_TO_KEEP_AFTER):
+                getFrame(i, cap)
+
             break
 
     return True
 
 
+def getFrame(index, cap):
+    ret, frame = cap.read()
+    name = "frames/frame%d.jpg" % index
+    cv2.imwrite(name, frame)
+
+
 def makeGif():
     print "Making gif"
+
     try:
         bashCommand = "convert -background white -alpha remove -layers OptimizePlus -delay 25x100 /home/pi/rewind/frames/frame*.jpg -loop 0 output.gif"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output = process.communicate()[0]
         print "Finished making gif"
         return True
+
     except KeyboardInterrupt:
         print "Interrupt dectected. Exiting"
         return False
