86c6babcf6c51e2605da0056b41c4b7102ef4102
Brady Smith <brady_smith@outlook.com>
2016-05-31 02:55:06 +0000

    recreating functionality

--- rewind.go
+++ rewind.go
@@ -1,22 +1,21 @@
-// Example program that uses blakjack/webcam library
-// for working with V4L2 devices.
-// The application reads frames from device and writes them to stdout
-// If your device supports motion formats (e.g. H264 or MJPEG) you can
-// use it's output as a video stream.
-// Example usage: go run stdout_streamer.go | vlc -
 package main
 
 import (
-	"github.com/blackjack/webcam"
 	"fmt"
-	"sort"
+	"github.com/blackjack/webcam"
 	"os"
-	_ "image/jpeg"
-	"strconv"
 	"reflect"
+	"sort"
+	"strconv"
+	"os/exec"
+	"bytes"
+	"io"
+	"image/gif"
 )
 
 type FrameSizes []webcam.FrameSize
+const FRAMES_TO_KEEP_BEFORE = 50
+const FRAMES_TO_KEEP_AFTER = 20
 
 func (slice FrameSizes) Len() int {
 	return len(slice)
@@ -32,6 +31,35 @@ func (slice FrameSizes) Less(i, j int) bool {
 //For sorting purposes
 func (slice FrameSizes) Swap(i, j int) {
 	slice[i], slice[j] = slice[j], slice[i]
+}
+
+func AddHuffmanTable(filename string) {
+	cmd := exec.Command("/usr/bin/python", "/home/pi/rewind/mjpeg.py", filename)
+	cmd.Run()
+}
+
+func MakeGif() {
+	cmd := exec.Command("/usr/bin/convert", "-delay", "15x100", "/home/pi/rewind/frames/frame*.jpg", "-loop", "0", "/home/pi/rewind/output.gif")
+	cmd.Run()
+}
+
+func UploadToSlack() {
+	c1 := exec.Command("echo 'Uploading gif. Please hold.'")
+	c2 := exec.Command("/usr/local/bin/slacker", "-c", "intersection-gifs", "-f", "/home/pi/rewind/output.gif")
+
+	r, w := io.Pipe()
+	c1.Stdout = w
+	c2.Stdin = r
+
+	var b2 bytes.Buffer
+	c2.Stdout = &b2
+
+	c1.Start()
+	c2.Start()
+	c1.Wait()
+	w.Close()
+	c2.Wait()
+	io.Copy(os.Stdout, &b2)
 }
 
 func main() {
@@ -60,8 +88,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Resulting image format: %s (%dx%d)\n", format_desc[f], w, h)
 	}
 
-	println("Press Enter to start streaming")
-	fmt.Scanf("\n")
 	err = cam.StartStreaming()
 	if err != nil {
 		panic(err.Error())
@@ -83,7 +109,7 @@ func main() {
 
 		frame, err := cam.ReadFrame()
 		if len(frame) != 0 {
-			var name = "./frames/image" + strconv.Itoa(i) + ".jpg"
+			var name = "/home/pi/rewind/frames/frame" + strconv.Itoa(i) + ".jpg"
 			fmt.Println(reflect.TypeOf(frame))
 			fo, err := os.Create(name)
 			if err != nil {
@@ -99,8 +125,10 @@ func main() {
 					panic(err)
 				}
 			}()
+
+			AddHuffmanTable(name)
 			i++
-			if (i > 5) {
+			if i > 5 {
 				i = 0
 			}
 
